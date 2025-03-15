f587aaf7a6931229e49643eaeb987c39cb6fe10e
Govind-3550 <govindvish2763@gmail.com>
2025-03-16 01:56:01 +0530

    final vercel build

--- backEnd/config/middlewares.js
+++ backEnd/config/middlewares.js
@@ -2,7 +2,18 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000', // Allow local development URL
+        'https://esy-com.vercel.app', // Your deployed frontend URL
+        'https://esycom.onrender.com', // Your deployed Strapi URL
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
