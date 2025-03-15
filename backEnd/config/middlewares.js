a9d64ded71d91d185e44e034ff62d3e63c85b901
Govind-3550 <govindvish2763@gmail.com>
2025-03-16 00:52:36 +0530

    middleware.js changes

--- backEnd/config/middlewares.js
+++ backEnd/config/middlewares.js
@@ -2,7 +2,17 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://esy-nutmb08gj-govind-3550s-projects.vercel.app/', // Replace with your actual Vercel frontend URL
+        'https://esycom.onrender.com', // Your Strapi production URL
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
