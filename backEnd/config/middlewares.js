e4069d02f7700bc4e192e5d00cec3e512691f329
Govind-3550 <govindvish2763@gmail.com>
2025-03-16 01:40:29 +0530

    reverting pre vercel version

--- backEnd/config/middlewares.js
+++ backEnd/config/middlewares.js
@@ -2,18 +2,7 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:3000', // Allow local development URL
-        'https://esy-com.vercel.app', // Your deployed frontend URL
-        'https://esycom.onrender.com', // Your deployed Strapi URL
-      ],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
